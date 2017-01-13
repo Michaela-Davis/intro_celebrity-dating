0d35b1edc1d32b0d717ccf381e12a9b214d081f6
Michaela Davis <michaela.delight@gmail.com>
2017-01-12 20:16:57 -0800

    add more logic with functioning alerts

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var age = parseInt($("#age").val());
     console.log(age);
-    // var gender = $(#gender).val();
+    var gender = $('input.gender:checked').val();
 
-    if (age >= 50) {
-      alert("Renee or Pierce");
-    } else if (age <= 30) {
-      alert("Emma or Liam");
+    if (age >= 50 && gender === "male") {
+      alert("Pierce");
+    } else if (age >= 50) {
+      alert("Renee");
     } else {
       alert ("Scarlett or Colin");
     }
